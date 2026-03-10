78e1efe2427c02d41790fb974056a988f23b3276
Skair <vlad.muckob>
2026-03-10 17:43:55 +0000

    first commit

---
+++ lw1IF_3.php
@@ -0,0 +1,12 @@
+<?php
+
+$income = rand(1,100000);
+echo "income: ".$income."\n";
+
+$nalog;
+if ($income <= 20000 && $income > 0) $nalog = $income * 10/100;
+elseif ($income > 20000 && $income <= 50000) $nalog = 2000 + ($income - 20000) * 15/100;
+elseif ($income > 50000) $nalog = 6500 + ($income - 50000) * 20/100;
+
+echo "Nalog: ".$nalog;
+?>
